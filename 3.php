a62b5dbe956f562874857673aea04fc2eeb84027
Slator-reps <Slator-reps>
2020-11-30 18:54:13 +0300

    First commit

---
+++ 3.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 'hhh';
+$$a = 'bbb';
+$$$a = 'hrrrrh';
+$$$$a = 'ooo';
+echo $a;
+echo '<br>';
+echo $$a;
+
+echo '<br>';
+echo $$$a;
+
+echo '<br>';
+echo $$$$a;
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$a = 'hhh';
+$$a = 'bbb';
+$$$a = 'hrrrrh';
+$$$$a = 'ooo';
+echo $a;
+echo '<br>';
+echo $$a;
+
+echo '<br>';
+echo $$$a;
+
+echo '<br>';
+echo $$$$a;
